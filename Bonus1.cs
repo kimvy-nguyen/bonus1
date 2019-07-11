d8ea1695a2550960ecd33463152f8e7a6b4e5ef5
KimVy <kimvy.ngu@gmail.com>
2019-07-11 17:03:03 -0400

    Bonus 1 Upload

---
+++ Bonus1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Bonus1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Letter Grade Converter!");
+            bool cont = true;
+            while (cont)
+            {
+                Console.Write("\nEnter a numerical grade: ");
+                int grade = Convert.ToInt32(Console.ReadLine());
+
+if (grade >= 88)
+                {
+                    Console.WriteLine("Letter Grade: A");
+                }
+                else if (87 >= grade && grade >= 80)
+                {
+                    Console.WriteLine("Letter Grade: B");
+                }
+                else if (79 >= grade && grade >= 67)
+                {
+                    Console.WriteLine("Letter Grade: C");
+                }
+                else if (66 >= grade && grade >= 60)
+                {
+                    Console.WriteLine("Letter Grade: C");
+                }
+                else
+                {
+                    Console.WriteLine("Letter Grade: F");
+                }
+
+                Console.Write("\nContinue?(y/n):");
+                string option = Console.ReadLine();
+
+                if (option == "Y" || option == "y")
+                {
+                    cont = true;
+                }
+                else if (option == "N" || option == "n")
+                {
+                    cont = false;
+                }
+
+            }
+
+                Console.WriteLine("\nGoodbye.");
+            }
+                
+             
+        
+    }
+}
